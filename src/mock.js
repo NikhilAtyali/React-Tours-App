acc98efa22e468e5be65e502b237e10a0cd59847
NikhilAtyali <88334264+NikhilAtyali@users.noreply.github.com>
2022-01-12 00:32:07 +0530

    Firts Commit

---
+++ src/mock.js
@@ -0,0 +1,13 @@
+const arr= [1,2,5,6,7,9,8,-4,-3]
+// const arr = [32, 33, -23, -25, -24, 44, 56]
+let result = [];
+
+    for (i=0;i>=0; i++){
+        if(i>0 && i%2==0 ){
+            result.push(i);
+            
+           
+            
+        }
+    }
+    console.log(result);
